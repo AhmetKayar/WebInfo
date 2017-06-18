e87a1008f533acedc673f6ae7d6ec1d94f4c56a7
Ahmet KAYAR <ahmet.kayar@gmail.com>
2017-06-18 16:28:39 +0300

    Panel Entegresi

---
+++ WebInfoApp/App.WebInfo.MVCUI/ViewComponents/MenuViewComponet.cs
@@ -0,0 +1,26 @@
+﻿using App.WebInfo.Business.Abstract;
+using App.WebInfo.MVCUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.WebInfo.MVCUI.ViewComponents
+{
+    public class MenuViewComponet: ViewComponent
+    {
+        private readonly IMenuService _menuService;
+        public MenuViewComponet(IMenuService menuService)
+        {
+            _menuService = menuService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() {
+            MenuViewModel view = new MenuViewModel()
+            {
+                list = await _menuService.GetList()
+            };
+            return View(view);
+        }
+    }
+}
